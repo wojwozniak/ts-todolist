1e83b243931a831e27ea004757f9de00d78b94f1
pulchnymamut <pulchnymamut@gmail.com>
2022-05-03 22:23:23 +0200

    Added input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <span className="heading">To Do List</span>
+      <span className="heading">Lista zadań</span>
+      <InputField />
     </div>
   );
 }
---
+++ src/components/InputField.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputField = () => {
+  return (
+      <form className="input">
+          <input type="input" placeholder="Wpisz zadanie" className='input__box' />
+        </form>
+  )
+}
+
+export default InputField
