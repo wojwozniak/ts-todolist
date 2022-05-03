02520585ca0bc75df5992d974ad7f452786d5b48
pulchnymamut <pulchnymamut@gmail.com>
2022-05-03 22:11:45 +0200

    Added stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <span className="heading">To Do List</span>
     </div>
   );
 }
