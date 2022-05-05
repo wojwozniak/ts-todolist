d259ffb36aaaa2e596e9397f86162693f8b4c7b1
pulchnymamut <pulchnymamut@gmail.com>
2022-05-05 15:34:21 +0200

    created TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import TodoList from './components/TodoList';
 import { Todo } from './model';
 
 const App: React.FC = () => {
@@ -20,7 +21,8 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <span className="heading">Lista zadań</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={ handleAdd }/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos ={todos} setTodos = {setTodos} />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import "./styles.css"
 
 interface Props {
@@ -7,10 +7,14 @@ interface Props {
   handleAdd: (e:React.FormEvent) => void;
 }
 
-const InputField = ({ todo, setTodo, handleAdd }:Props) => {
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-      <form className="input" onSubmit={handleAdd}>
-      <input type="input" value={todo} onChange={
+    <form className="input" onSubmit={(e) => {
+      handleAdd(e);
+      inputRef.current?.blur();
+    }}>
+      <input ref={inputRef} type="input" value={todo} onChange={
         (e)=>setTodo(e.target.value)
       }
         placeholder="Wpisz zadanie" className='input__box' />
