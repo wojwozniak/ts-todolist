ce0688759fac9f9b55f30745374b6a6f1f7cc4ea
pulchnymamut <pulchnymamut@gmail.com>
2022-05-06 00:11:47 +0200

    Added focus on input

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Todo } from '../model'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
@@ -32,10 +32,17 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
         setEdit(false);
     };
 
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, [edit]);
+
+
     return (
         <form className="todos__single" onSubmit={(e)=>handleEdit(e, todo.id)}>
             {edit ? (
-                <input value={editTodo} onChange={(e) => setEditTodo(e.target.value)} className='todos__single--text'/>
+                <input ref={ inputRef } value={editTodo} onChange={(e) => setEditTodo(e.target.value)} className='todos__single--text'/>
             ): (
                     todo.isDone ? (
                         <s className="todos__single--text"> {todo.todo} </s>
