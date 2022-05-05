7517d8ccd81367d68400445544c8370207cc3799
pulchnymamut <pulchnymamut@gmail.com>
2022-05-05 17:17:20 +0200

    single todo component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Todo } from '../model';
+import SingleTodo from './SingleTodo';
 import "./styles.css";
 
 interface Props {
@@ -10,8 +11,13 @@ interface Props {
 const TodoList:React.FC<Props> = ({todos,setTodos}) => {
   return (
     <div className="todos">
-          {todos.map(todo => (
-              <li>{todo.todo}</li>
+          {todos.map((todo) => (
+            <SingleTodo
+              todo={todo}
+              key={todo.id}
+              todos={todos}
+              setTodos={setTodos}
+            />
         ))}
     </div>
   )
